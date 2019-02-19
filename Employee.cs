fc8746840dad00cbf855e29efa480b0d37285f5b
AnnZh <ann.zhurok@gmail.com>
2019-02-19 22:15:54 +0300

    Done

---
+++ Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        private string surname;
+        private int age;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        private string GetEmployeeAge()
+        {
+            return age.ToString();
+        }
+
+        public void ChangeEmployeeSurname(string surname)
+        {
+            this.surname = surname;
+        }
+
+        public string InfoAboutEmployee()
+        {
+            return "Фамилия: { Employee.surname}, Возраст: { Employee.age}";
+        }
+    }
+}
